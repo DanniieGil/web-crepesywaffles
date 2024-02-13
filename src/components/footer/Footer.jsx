3162abc2bc2688f9c4b23d8ddae2374e8ed3566b
Daniel Gil <danniegilr@gmail.com>
2024-02-13 09:06:19 -0500

    Fix imports on Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import styles from "./footer.module.css"
-import Links from "./links/links"
-import SocialMedia from "./socialmedia/socialmedia"
+import Links from "./links/Links"
+import SocialMedia from "./socialmedia/Socialmedia"
 import Image from "next/image"
 
 export default function Footer() {
