9fc1d18bbaf8732c07087509c8fafdefdfd15242
Daniel Gil <danniegilr@gmail.com>
2024-02-13 14:59:12 -0500

    Completed home v1

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,49 +1,85 @@
 import styles from "./page.module.css"
-import Link from "next/link"
+import Link from "next/link";
+import Image from "next/image"
+
+export const metadata = {
+  title: 'Crepes & Waffles - Arte transformado en alimento.',
+  description: "Arte transformado en alimento. Naturalmente trabajando, artesanalmente produciendo, saludablemente complaciendo.",
+};
 
 export default function Home() {
   return ( 
-    <div className={styles.container}>
+    <div className={styles.container}> 
+
       <div className={styles.section_one}>
         <h1>Hola</h1>
         <h4 className={styles.h4_simple}>
-          Somos Crepes&Waffles y estamos 
-          Naturalmente trabajando, artesanalmente produciendo, 
+          Somos Crepes&Waffles y estamos<br/>
+          Naturalmente trabajando, artesanalmente produciendo,<br/>
           saludablemente complaciendo.
         </h4>
       </div>
       <div className={styles.section_two}>
         <h4>
-          <strong>En Crepes & Waffles sabemos que la mesa y el comer son actos de amor,</strong> que involucran todos los sentidos, 
-          por eso, ponemos gran empeño en el servicio y mucha atención en la selección del mobiliario y de las luces,
-          para generar lugares con ambiente, donde los alimentos, con sus formas, sus texturas, sus colores, sus aromas 
+          En Crepes & Waffles sabemos que la mesa y el comer son actos de amor, que involucran todos los sentidos, <br/> 
+          por eso, ponemos gran empeño en el servicio y mucha atención en la selección del mobiliario y de las luces,<br/>
+          para generar lugares con ambiente, donde los alimentos, con sus formas, sus texturas, sus colores, sus aromas <br/>
           y sabores, complazcan todos los deseos.
         </h4>
       </div>
       <div className={styles.section_three}>
-        <h2>NOSOTROS</h2>
-        <h3>¿QUE SOMOS</h3>
-        <Link href="/" className={styles.show_more_btn}>VER MÁS</Link>
+        <div className={styles.col_1}>
+          <h2>NOSOTROS</h2>
+          <h3>¿QUE SOMOS?</h3>
+          <Link href="/" className={styles.show_more_btn}>VER MÁS</Link>
+        </div>
+        <div className={styles.col_2}>
+          <Image src="/assets/img_1.jpg" width={769} height={722}/>
+        </div>
       </div>
       <div className={styles.section_four}>
-        <h2>BONOS</h2>
-        <h3>DE REGALO</h3>
-        <Link href="/" className={styles.show_more_btn}>VER MÁS</Link>
+        <div className={styles.request_here_btn}>
+          <Link href="https://crepesywaffles.com/bonos">
+            <Image src="/assets/pide_aqui_2.svg" width={78} height={285}/>
+          </Link>
+        </div>
+        <div className={styles.col_1}>
+          <h2>BONOS</h2>
+          <h3>DE REGALO</h3>
+          <Link href="/" className={styles.show_more_btn}>VER MÁS</Link>
+        </div>
+        <div className={styles.col_2}>
+          <Image src="/assets/img_2.jpg" width={769} height={722}/>
+        </div>
       </div>
       <div className={styles.section_five}>
+        <div className={styles.request_here_btn}>
+          <Link href="https://crepesywaffles.com/bonos">
+            <Image src="/assets/pide_aqui_1.svg" width={78} height={285}/>
+          </Link>        </div>
         <h2>COEXISTO</h2>
         <h3>LO QUE HACEMOS</h3>
         <Link href="/" className={styles.show_more_btn}>VER MÁS</Link>
       </div>
       <div className={styles.section_six}>
-        <h2>CREPES</h2>
-        <h3>EN CASA</h3>
-        <Link href="/" className={styles.show_more_btn}>VER MÁS</Link>
+        <div className={styles.col_1}>
+          <h2>CREPES</h2>
+          <h3>EN CASA</h3>
+          <Link href="/" className={styles.show_more_btn}>VER MÁS</Link>
+        </div>
+        <div className={styles.col_2}>
+          <Image src="/assets/img_3.jpg" width={769} height={722}/>
+        </div>
       </div>
       <div className={styles.section_seven}>
-        <h2>NOSOTROS</h2>
-        <h3>EN EL MUNDO</h3>
-        <Link href="/" className={styles.show_more_btn}>VER MÁS</Link>
+        <div className={styles.col_1}>
+          <h2>NOSOTROS</h2>
+          <h3>EN EL MUNDO</h3>
+          <Link href="/" className={styles.show_more_btn}>VER MÁS</Link>
+        </div>
+        <div className={styles.col_2}>
+          <Image src="/assets/img_4.jpg" width={769} height={722}/>
+        </div>
       </div>
     </div> 
   )
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,27 +1,53 @@
-"use client"
-import Link from "next/link"
-import Links from "./links/Links"
-import Image from "next/image"
-import styles from "./navbar.module.css"
-import { useContext } from "react"
-import {SidebarContext} from "@/context/SidebarContext";
- 
-export default function Navbar() {
 
+import Link from "next/link";
+import Links from "./links/Links";
+import Image from "next/image";
+import styles from "./navbar.module.css";
+import { useContext, useState, useEffect } from "react";
+import { SidebarContext } from "@/context/SidebarContext";
+
+export default function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+  const [isClient, setIsClient] = useState(false);
   const { toggleSidebarcollapse } = useContext(SidebarContext);
 
+  useEffect(() => {
+    setIsClient(true);
+
+    const handleScroll = () => {
+      const isScrolled = window.scrollY >= 200;
+      setScrolled(isScrolled);
+    };
+
+    const checkInitialScrollPosition = () => {
+      const isScrolled = window.scrollY >= 200;
+      setScrolled(isScrolled);
+    };
+
+    if (typeof window !== 'undefined') {
+      checkInitialScrollPosition();
+      window.addEventListener('scroll', handleScroll);
+
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }
+  }, []);
+
+  const containerStyle = isClient && (scrolled || typeof window === 'undefined') ? styles.scrolled : '';
+
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${containerStyle}`}>
       <div className={styles.logos}>
-        <button onClick={toggleSidebarcollapse} >
-          <Image src="assets/hamburger.svg" width={40} height={21}/>
+        <button onClick={toggleSidebarcollapse}>
+          <Image src={`${containerStyle ? "assets/hamburger_2.svg" : "assets/hamburger.svg"}`} width={40} height={21} />
         </button>
         <Link href="/">
-          <Image src="assets/logo.svg" width={422} height={36}/>
+          <Image src={`${containerStyle ? "assets/logo_2.svg" : "assets/logo.svg"}`} width={422} height={36} />
         </Link>
       </div>
       <div>
-        <Links/>
+        <Links />
       </div>
       <div className={styles.col_3}>
         <Link href="https://domicilios.crepesywaffles.com/ingresar/?next=/ingresar/" className={styles.login_btn}>
