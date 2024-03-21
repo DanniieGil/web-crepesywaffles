65f6d9a27118e471e0035f5af92bf59a193fa638
Daniel Gil <danniegilr@gmail.com>
2024-03-21 13:51:19 -0500

    completed nosotros

--- src/app/nosotros/page.jsx
+++ src/app/nosotros/page.jsx
@@ -18,7 +18,9 @@ export default function Nosotros() {
       </div>
       <div className={styles.section_two}>
         <div className={styles.line_1}>MISIÓN</div>
-        <div className={styles.line_2}>Despertar admiración en nuestra comunidad</div>
+        <div className={styles.line_2}>
+          Despertar admiración en nuestra comunidad
+        </div>
         <div className={styles.line_3}>por regenerar vida</div>
         <div className={styles.line_4}>y servir arte-sano con amor</div>
         <div className={styles.line_5}>y alegría a precios razonables.</div>
@@ -27,26 +29,24 @@ export default function Nosotros() {
         <div className={styles.title}>VISIÓN</div>
         <div className={styles.description}>
           Hacer de Crepes & Waffles una empresa líder en lo que realiza,
-          despertando afecto y sentido de pertenencia entre todos sus clientes, 
+          despertando afecto y sentido de pertenencia entre todos sus clientes,
           empleados, colaboradores y la comunidad en general.
         </div>
       </div>
       <div className={styles.section_four}>
         <div className={styles.title}>VALORES</div>
         <div className={styles.description}>
-          Nos esforzamos por construir una mejor empresa a través de 
-          nuestras acciones y decisiones diarias. Descubre cómo 
-          nuestros valores corporativos dan forma a todo lo que hacemos.
+          Nos esforzamos por construir una mejor empresa a través de nuestras
+          acciones y decisiones diarias. Descubre cómo nuestros valores
+          corporativos dan forma a todo lo que hacemos.
         </div>
       </div>
       <div className={styles.section_five}>
         <div className={styles.title}>INNOVACIÓN</div>
         <div className={styles.description}>
-          Con la idea de llegar al corazón y quedarnos en la mente, 
-          nos reinventamos todo el tiempo con la creación de nuevos platos 
-          y la mezcla de diferentes ingredientes que juntos forman una 
-          sinfonía capaz de cautivar los paladares más conocedores y los 
-          apetitos más ambiciosos.
+          Con la idea de llegar al corazón y quedarnos en la mente, nos
+          reinventamos todo el tiempo con la creación de nuevos platos y la mezcla de diferentes ingredientes que juntos forman una sinfonía capaz de cautivar los paladares más conocedores y los apetitos más
+          ambiciosos.
         </div>
       </div>
       <div className={styles.section_six}>
@@ -56,5 +56,5 @@ export default function Nosotros() {
         </div>
       </div>
     </div>
-  )
+  );
 }
